88306df30269e1acb50b92b64264278791e8f8e5
BuildTools <darcros7@gmail.com>
2019-08-23 15:30:07 +0200

    throw apollo errors so error code is correct

--- src/modules/authentication/authChecker.ts
+++ src/modules/authentication/authChecker.ts
@@ -1,18 +1,22 @@
 import { AuthChecker } from 'type-graphql';
+import { AuthenticationError, ForbiddenError } from 'apollo-server-express';
+
 import { Context } from '../../util/context.interface';
 import { Role } from '../../entity/user.entity';
 
 export const customAuthChecker: AuthChecker<Context, Role> = ({ context: { user } }, roles) => {
   // If the user is not logged in
-  if (!user) return false;
+  if (!user) {
+    throw new AuthenticationError('Not authenticated');
+  }
 
   // If `@Authorized()` without roles
-  if (roles.length == 0) return true;
+  if (roles.length === 0) return true;
 
   // Check if user role is present in roles
   if (roles.includes(user.role)) {
     return true;
   }
 
-  return false;
+  throw new ForbiddenError('Action not permitted');
 };
