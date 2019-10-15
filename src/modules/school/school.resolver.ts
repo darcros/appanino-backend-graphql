4bc5d039bf6317f7aea48c6e41a2f77b4fc0ca3b
Dario Crosa <darcros7@gmail.com>
2019-10-15 21:33:45 +0200

    made school list public

--- src/modules/school/school.resolver.ts
+++ src/modules/school/school.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Authorized } from 'type-graphql';
+import { Resolver, Query, Authorized, FieldResolver, Root } from 'type-graphql';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { SchoolRepository } from './school.repository';
 import { School } from '../../entity/school.entity';
@@ -9,9 +9,20 @@ export class SchoolResolver {
   @InjectRepository(SchoolRepository)
   private readonly schoolRepository: SchoolRepository;
 
-  @Authorized(Role.Admin, Role.SchoolAdmin, Role.User)
   @Query(() => [School])
   public async schools() {
     return await this.schoolRepository.find();
+  }
+
+  @Authorized(Role.Admin, Role.SchoolAdmin)
+  @FieldResolver()
+  public async users(@Root() school: School) {
+    return school.users;
+  }
+
+  @Authorized(Role.Admin, Role.SchoolAdmin, Role.User)
+  @FieldResolver()
+  public async products(@Root() school: School) {
+    return school.products;
   }
 }
